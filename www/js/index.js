2d606e913ec4592533b90d239700ca3589e65a28
Sharvan <itbhu.sk@gmail.com>
2017-10-15 17:09:28 +0530

    b

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
 		 //window.open = cordova.InAppBrowser.open;
+		 StatusBar.hide
 		 openScreen('home');
         app.receivedEvent('deviceready');	
     },
